5c3403d000b42956c2cd141819888ba176e3ab96
KODAK-CODER <kamuridon@gmail.com>
2026-05-14 09:55:08 +0300

    chore: model training functions and imports

--- src/model_training.py
+++ src/model_training.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
 
 # Importing project-wide constants
-import src.constants as c
+import constants as c
 
 def train_kmeans(
     X: pd.DataFrame, 
